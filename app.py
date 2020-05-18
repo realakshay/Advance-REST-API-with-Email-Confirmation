c777b79624a5a3dfb5fc9f83480c9202fcf4088b
realakshay <gaikwadakshay663@gmail.com>
2020-05-18 10:09:48 +0530

    Manage : Here security of small things maintain in environment

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
+from flask_restful import Api
 from db import db
 from ma import ma
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('APP_SECRET_KEY')
+api = Api(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 if __name__ == '__main__':
     db.init_app(app)
