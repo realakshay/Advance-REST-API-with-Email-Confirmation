a8f18ed8ac5aefeaa9fbf166825f701ef8f34eed
realakshay <gaikwadakshay663@gmail.com>
2020-05-18 09:26:47 +0530

    Modify : Here important db and ma objects import and initialize

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from db import db
+from ma import ma
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
+    db.init_app(app)
+    ma.init_app(app)
     app.run(debug=True)
